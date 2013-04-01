37eb0588b9652bf7c23d6667ec3a3530f15d0c45
Andrew Williams <andy@tensixtyone.com>
2013-04-01 01:02:53 +0100

    Further refinements to Heroku Support.

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
-web: python manage.py run_gunicorn
+web: cd app; python manage.py collectstatic --noinput; python manage.py run_gunicorn
+debug: cd app; python manage.py collectstatic --noinput; DJANGO_DEBUG=1 python manage.py run_gunicorn
+update_haystack: cd app; python manage.py update_index
+reindex_haystack: cd app; python manage.py rebuild_index --noinput
+syncdb: cd app; python manage.py syncdb --migrate --noinput
--- app/vapemap/settings.py
+++ app/vapemap/settings.py
@@ -27,10 +27,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', '..', 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), '..', '..', 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '..', 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
