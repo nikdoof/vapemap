4a7d009483bf68e0a5ed85f32ebcbfa091cdc935
Andrew Williams <andy@tensixtyone.com>
2013-04-02 01:20:33 +0100

    Make the wording a little more international friendly.

--- app/stores/models.py
+++ app/stores/models.py
@@ -142,9 +142,9 @@ class Address(models.Model):
     address2 = models.CharField('Address Line 2', max_length=200, blank=True, help_text="(Optional) Second line of the address")
     address3 = models.CharField('Address Line 3', max_length=200, blank=True, help_text="(Optional) Third line of the address")
     city = models.CharField('Town/City', max_length='50', help_text="City or town")
-    county = models.ForeignKey('stores.County', related_name='addresses', null=True, blank=True, help_text="County or suburban area.")
+    county = models.ForeignKey('stores.County', related_name='addresses', null=True, blank=True, help_text="County or suburban area. (Ignore if the store is outside the UK)")
     country = models.ForeignKey('stores.Country', related_name='addresses')
-    postcode = models.CharField('Postcode', max_length=20, help_text="Post Code, e.g. <em>M1 1AA</em>")
+    postcode = models.CharField('Postcode / Zipcode', max_length=20, help_text="Post Code or Zip Code, e.g. <em>M1 1AA</em> or <em>60647</em>")
 
     geo_latitude = models.FloatField('Latitude', null=True, blank=True)
     geo_longitude = models.FloatField('Longitude', null=True, blank=True)
