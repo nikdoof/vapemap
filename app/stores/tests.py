b3e01bfde9a0a66d174f68bb728b6bb9870ddedd
Andrew Williams <andy@tensixtyone.com>
2013-04-04 21:05:46 +0100

    Added simplistic view tests.

---
+++ app/stores/tests.py
@@ -0,0 +1,29 @@
+from django.utils import unittest
+from django.core.urlresolvers import reverse
+
+
+class StoreViewsTestCase(unittest.TestCase):
+
+    def test_map_index(self):
+        resp = self.client.get(reverse('map'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_store_list(self):
+        resp = self.client.get(reverse('store-list'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_chain_list(self):
+        resp = self.client.get(reverse('chain-list'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_chain_claim(self):
+        resp = self.client.get(reverse('chain-claim'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_store_claim(self):
+        resp = self.client.get(reverse('store-claim'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_store_update(self):
+        resp = self.client.get(reverse('store-update'))
+        self.assertEqual(resp.status_code, 200)
