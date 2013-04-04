74764bae3233c472148eff066f8677c2810c87f4
Andrew Williams <andy@tensixtyone.com>
2013-04-04 20:28:11 +0100

    Added basic Jenkins testing support.

---
+++ app/vapemap/conf/test.py
@@ -0,0 +1,17 @@
+from .base import *
+
+INSTALLED_APPS += [
+    'django-jenkins',
+]
+
+PROJECT_APPS = [
+    'stores',
+]
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+)
